989633de9a86566091c17ddf19527bd54437cf5d
kummel <rafaelktmendes@gmail.com>
2016-06-21 11:07:30 -0300

    Gas e audio
    Gas modificado e audio modificados

--- Arduino/Gas/Gas.ino
+++ Arduino/Gas/Gas.ino
@@ -4,15 +4,40 @@
    Dev: Michalis Vasilakis // Date: 11/6/2015 // www.ardumotive.com                     */
 
 const int gasPin = A0; //GAS sensor output pin to Arduino analog A0 pin
-
+int val2 = 0;
+int leitorFumaca = 0;
+int pwmPin = 3;
 
 void setup()
 {
+  pinMode(pwmPin, OUTPUT); // sets the pin as output
+  TCCR3B = (TCCR3B & 0b11111000) | 0x04;
+
   Serial.begin(9600); //Initialize serial port - 9600 bps
 }
 
 void loop()
 {
   Serial.println(analogRead(gasPin));
-  delay(1000); // Print value every 1 sec.
+  leitorFumaca = analogRead(gasPin);
+  if (leitorFumaca > 250) {
+    digitalWrite(releLuz2, LOW);
+    if (val2 == 0) {
+      flag = 1;
+    } else if (val2 == 510 ) {
+      flag = 0;
+    }
+
+    analogWrite(pwmPin, (val2));
+
+    if (flag == 1) {
+      val2++;
+    } else {
+      val2--;
+    }
+    delay(7);
+  }
+  else {
+    digitalWrite(pwmPin, LOW);
+  }
 }
